5109dd101c9ae15ab086f5cd1ce273e26fddb185
Saran Mahadev <sarandevnet@gmail.com>
2021-01-17 17:26:56 +0530

    Added Songs

--- upload.py
+++ upload.py
@@ -7,11 +7,13 @@ data = json.load(open('data.json','r'))
 for filenames in os.walk('songs'):
     for song in filenames[2]:
         time = MP3('songs/'+song).info.length
-        data["songs"].append({
+        songdict = {
             "link" :  "http://saranmahadev.tech/music/songs/"+song,
             "song" : song.split('.')[0],
             "time" : str(round(time/60,2))
-        })      
+        }
+        if songdict not in data["songs"]:
+            data["songs"].append(songdict)      
 
 with open('data.json','w') as outfile:
     json.dump(data,outfile)
